ee0d1bfafe81035200c98c32522982b62997366f
Jindra Petřík <jindra.petrik@gmail.com>
2018-01-10 22:08:43 +0100

    travis script change

--- travis/script.sh
+++ travis/script.sh
@@ -2,9 +2,10 @@
 # If we've got website password, we can upload nightly builds.
 # Travis secure variable $website_password is not available from outside 
 # of jpexs repository (e.g pull requests from other users on GitHub)
-if [ -z ${github_password+x} ]; then
+if [ -z ${GITHUB_ACCESS_TOKEN+x} ]; then
     # password not set,  just make private release without publishing result
     #...
+    echo "no github access token set"
 else
     # if we are on the dev branch
     if [ $TRAVIS_BRANCH = "dev" ]; then
@@ -14,5 +15,6 @@ else
     else
         # another branch, just make private release
         #...
+        echo "NOT ON dev branch"
     fi
 fi

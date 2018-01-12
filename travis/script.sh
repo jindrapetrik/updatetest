1a7591a21750ede2a27dfa82b4989ca0ea8674df
Jindra Petřík <jindra.petrik@gmail.com>
2018-01-12 21:15:50 +0100

    fix

--- travis/script.sh
+++ travis/script.sh
@@ -9,15 +9,11 @@ if [ -z ${GITHUB_ACCESS_TOKEN+x} ]; then
     #...
     echo "no github access token set"
 else
-    git tag -d nightly1
-    git remote add myorigin https://${GITHUB_ACCESS_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git > /dev/null 2>&1
-    git push --quiet myorigin :refs/tags/nightly1
     # if we are on the dev branch
-    if [ $TRAVIS_BRANCH = "devxxx" ]; then
+    if [ $TRAVIS_BRANCH = "dev" ]; then
         # create nightly build...
                 
         TAG_COMMIT_HASH=$TRAVIS_COMMIT
-        GITHUB_USER=jindrapetrik
         GITHUB_REPO=$TRAVIS_REPO_SLUG
         RELEASES_JSON=`curl --silent --request GET --header "Accept: application/vnd.github.manifold-preview" --user $GITHUB_USER:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO/releases`
         LAST_NIGHTLY_VER=`echo $RELEASES_JSON|jq --raw-output '.[].tag_name'|grep 'nightly'|sed 's/nightly//'|head -n 1`
@@ -26,8 +22,8 @@ else
         NEXT_NIGHTLY_TAG=nightly$NEXT_NIGHTLY_VER
         
         #Release new nightly
-        #TAGGER_NAME="Jindra Pet\u0159ik"
-        TAGGER_EMAIL=jindra.petrik@gmail.com
+        TAGGER_NAME="Travis CI"
+        TAGGER_EMAIL=travis@travis-ci.org
         VERSION_NAME="nightly $NEXT_NIGHTLY_VER"
         VERSION_DESCRIPTION="Nightly version $NEXT_NIGHTLY_VER with some changes"
         VERSION_PRERELEASE=true
@@ -35,8 +31,7 @@ else
 
         ESC_VERSION_NAME=`echo $VERSION_NAME|jq --raw-input --ascii-output '.'`
         ESC_VERSION_DESCRIPTION=`echo $VERSION_DESCRIPTION|jq --raw-input --ascii-output '.'`
-        ESC_TAGGER_NAME='"Jindra Pet\u0159ik"'
-        #`echo $TAGGER_NAME|jq --raw-input --ascii-output '.'`        
+        ESC_TAGGER_NAME=`echo $TAGGER_NAME|jq --raw-input --ascii-output '.'`        
         TAG_NAME=$NEXT_NIGHTLY_TAG
         VERSION_PRERELEASE=true
         echo TAG_NAME=$TAG_NAME
@@ -53,9 +48,17 @@ else
         
         #Remove old nightly
         echo "Removing old nightly..."
+        #-remove release
         TAG_INFO=`curl --silent --user $GITHUB_USER:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO/releases/tags/$LAST_NIGHTLY_TAG`
         RELEASE_ID=`echo $TAG_INFO|jq '.id'`
         curl --silent --request DELETE --user $GITHUB_USER:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO/releases/$RELEASE_ID >/dev/null
+        #-delete tag
+        git config --global user.email "$TAGGER_EMAIL"
+        git config --global user.name "$TAGGER_NAME"
+        git tag -d $LAST_NIGHTLY_TAG
+        git remote add myorigin https://${GITHUB_ACCESS_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git > /dev/null 2>&1
+        git push --quiet myorigin :refs/tags/$LAST_NIGHTLY_TAG > /dev/null 2>&1
+    
         
         echo "FINISHED"
     else

42b9739bca924124dda81f0ac117cefb5df86371
Jindra Petřík <jindra.petrik@gmail.com>
2018-01-13 09:04:33 +0100

    echo current branch on deploy

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -3,6 +3,10 @@
 set -e
 git config --local user.name "jindrapetrik"
 git config --local user.email "jindra.petrik@gmail.com"
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo CURRENT_BRANCH=$CURRENT_BRANCH
+ 
 if [ -n "$DEPLOY_CREATE_TAG" ]; then
+  echo DEPLOY_CREATE_TAG=$DEPLOY_CREATE_TAG
   git tag "$DEPLOY_CREATE_TAG" 
 fi

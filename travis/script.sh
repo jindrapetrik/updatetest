e64771ad2ecda423f2868092bc8a9db3e3338642
Jindra Petřík <jindra.petrik@gmail.com>
2018-01-13 15:21:16 +0100

    nightly version name has latest version in its name

--- travis/script.sh
+++ travis/script.sh
@@ -33,30 +33,28 @@ else
                   
           TAG_COMMIT_HASH=$TRAVIS_COMMIT
           GITHUB_REPO=$TRAVIS_REPO_SLUG
+          echo "Getting new version tag and name..."
           RELEASES_JSON=`curl --silent --request GET --header "Accept: application/vnd.github.manifold-preview" --user $GITHUB_USER:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO/releases`
           LAST_NIGHTLY_VER=`echo $RELEASES_JSON|jq --raw-output '.[].tag_name'|grep 'nightly'|sed 's/nightly//'|head -n 1`
+          LAST_STABLE_VER=`echo $RELEASES_JSON|jq --raw-output '.[].tag_name'|grep 'version'|sed 's/version//'|head -n 1`
           NEXT_NIGHTLY_VER=$(($LAST_NIGHTLY_VER+1))
           LAST_NIGHTLY_TAG=nightly$LAST_NIGHTLY_VER
           NEXT_NIGHTLY_TAG=nightly$NEXT_NIGHTLY_VER
           
-          #Release new nightly
-          VERSION_NAME="nightly $NEXT_NIGHTLY_VER"
-          VERSION_DESCRIPTION="Nightly version $NEXT_NIGHTLY_VER with some changes"
-          VERSION_PRERELEASE=true
           CURRENT_DATE=`date +%Y-%m-%dT%H:%M:%SZ`
   
           ESC_TAGGER_NAME=`echo $TAGGER_NAME|jq --raw-input --ascii-output '.'`        
           TAG_NAME=$NEXT_NIGHTLY_TAG
           VERSION_PRERELEASE=true
                   
-          #-create tag
+          #Create tag
           echo "Creating tag $TAG_NAME..."
           echo '{"tag":"'$TAG_NAME'","message":"","object":"'$TAG_COMMIT_HASH'","type":"commit","tagger":{"name":'$ESC_TAGGER_NAME',"email":"'$TAGGER_EMAIL'","date":"'$CURRENT_DATE'"}}'>json.bin
           curl --silent --request POST --data-binary @json.bin --header "Content-Type: application/json" --header "Accept: application/vnd.github.manifold-preview" --user $GITHUB_USER:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO/git/tags>/dev/null
-                    
+                              
           export DEPLOY_RELEASE_TO_REMOVE=$LAST_NIGHTLY_TAG                                 
           export DEPLOY_TAG_NAME=$NEXT_NIGHTLY_TAG
-          export DEPLOY_VERSION_NAME="nightly $NEXT_NIGHTLY_VER"
+          export DEPLOY_VERSION_NAME="version $LAST_STABLE_VER nightly $NEXT_NIGHTLY_VER"
           export DEPLOY_DESCRIPTION=`php ./travis/extract_changelog.php Unreleased`
           export DEPLOY_COMMITISH="dev"
           export DEPLOY_PRERELEASE=true
